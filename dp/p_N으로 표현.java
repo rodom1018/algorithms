9fb23de59a9061c270262133967b695862903dc6
rodom_macbook <enjoying1018@naver.com>
2024-12-07 23:01:47 +0900

    프로그래머스 - N으로 표현

--- dp/p_N으로 표현.java
+++ dp/p_N으로 표현.java
@@ -4,28 +4,49 @@ class Solution {
     public int solution(int N, int number) {
         
         //초기화 
-        ArrayList<Set<Integer>>[] list = new ArrayList[9];
+        int answer = -1;
+        ArrayList<Set<Integer>> list = new ArrayList<>();
         
         for(int i=0;i<9; i++){
-            list[i] = new HashSet<>();
+            list.add(new HashSet<>());
         }
         
         // 1번~9번 써서 나올수 있는 숫자를 수집
         for(int i=1; i<9; i++){
+            
+            Set<Integer> curSet = list.get(i);
+            
             for(int a=1; a<i ; a++){
+                int b = i - a;
                 
+                Set<Integer> a_set = list.get(a);
+                Set<Integer> b_set = list.get(b);
+                
+                
+                for(int now_a : a_set){
+                    for(int now_b: b_set){
+                        curSet.add(now_a + now_b);
+                        curSet.add(now_a - now_b);
+                        curSet.add(now_a * now_b);
+                        
+                        if(now_b != 0){
+                            curSet.add(now_a / now_b);
+                        }
+                    }
+                }
             }
             
             
             
             // 5, 55, 555, 5555와 같은 숫자 추가 
-            String temp = "";
-            for(int t=0 t<=i; t++){
-                temp = temp + (String) number;
+            curSet.add(Integer.parseInt(Integer.toString(N).repeat(i)));
+            
+            if(curSet.contains(number)){
+                answer = i;
+                break;
             }
             
         }
-        int answer = 0;
         return answer;
     }
 }
